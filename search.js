40ec7791493103b6c60f19784e269d73c98c3331
rajatdeveloper <singhal.rajat97@gmail.com>
2018-01-07 12:54:14 +0530

    Task Completed

---
+++ search.js
@@ -0,0 +1,9 @@
+(function() {
+    var cx = '004574096502876159299:fuonpbxuewq';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
